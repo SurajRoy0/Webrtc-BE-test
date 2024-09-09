2a1f938c6a989e4e0e3a5b47f8d84401d299d58b
SurajRoy0 <myself.suraj0@gmail.com>
2024-09-09 22:39:41 +0530

    response message added to check api

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
-import { Server } from "socket.io";
+import http from 'http';
+import { Server } from 'socket.io';
 
-const server = http.createServer();
+// Create HTTP server
+const server = http.createServer((req, res) => {
+  if (req.url === '/status') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Server is running');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+});
+
+// Create Socket.io server
 const io = new Server(server, {
   cors: {
     origin: "*", 
@@ -8,44 +20,50 @@ const io = new Server(server, {
     allowedHeaders: ["Content-Type"],
   },
 });
-const emailToSocketIdMap = new Map();
-const socketIdToEmailMap = new Map()
 
+const emailToSocketIdMap = new Map();
+const socketIdToEmailMap = new Map();
 
 io.on('connection', (socket) => {
-    console.log("Connection Done!");
-    socket.on('room:join', data => {
-        console.log('data: ',data)
-        const {email, room} = data
-        console.log(email, room,  "Room Join")
+  console.log("Connection Done!");
 
-        emailToSocketIdMap.set(email, socket.id);
-        socketIdToEmailMap.set(socket.id, email);
-      
+  socket.on('room:join', (data) => {
+    console.log('data: ', data);
+    const { email, room } = data;
+    console.log(email, room, "Room Join");
 
-        io.to(room).emit('user:joined', {email, socketId: socket.id});
-        socket.join(room);
+    emailToSocketIdMap.set(email, socket.id);
+    socketIdToEmailMap.set(socket.id, email);
 
-        io.to(socket.id).emit('room:join', data)
-    });
+    io.to(room).emit('user:joined', { email, socketId: socket.id });
+    socket.join(room);
 
-    socket.on('outgoing:call', data => {
-        const {to, offer} = data;
-        io.to(to).emit('incoming:call', {from: socket.id, offer})
-    })
+    io.to(socket.id).emit('room:join', data);
+  });
 
-    socket.on('call:accepted', data => {
-        const {to, answer} = data;
-        io.to(to).emit('call:accepted', {from: socket.id, answer})
-    });
+  socket.on('outgoing:call', (data) => {
+    const { to, offer } = data;
+    io.to(to).emit('incoming:call', { from: socket.id, offer });
+  });
 
-    socket.on('peer:negotiation:needed', data => {
-        const {to, offer} = data;
-        io.to(to).emit('peer:negotiation:needed', {from: socket.id, offer})
-    });
+  socket.on('call:accepted', (data) => {
+    const { to, answer } = data;
+    io.to(to).emit('call:accepted', { from: socket.id, answer });
+  });
 
-    socket.on('peer:negotiation:done', data => {
-        const {to, answer} = data;
-        io.to(to).emit('peer:negotiation:final', {from: socket.id, answer})
-    })
-})
+  socket.on('peer:negotiation:needed', (data) => {
+    const { to, offer } = data;
+    io.to(to).emit('peer:negotiation:needed', { from: socket.id, offer });
+  });
+
+  socket.on('peer:negotiation:done', (data) => {
+    const { to, answer } = data;
+    io.to(to).emit('peer:negotiation:final', { from: socket.id, answer });
+  });
+});
+
+// Start the server
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
